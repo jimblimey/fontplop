d060e0771738176086be8025d5c9c7483f86d2cc
Jim Blimey <jimblimey@outlook.com>
2022-11-24 21:35:14 +0000

    Added character edit code

--- main.pas
+++ main.pas
@@ -20,6 +20,8 @@ type
     procedure btnLoadClick(Sender: TObject);
     procedure buttonPanelResize(Sender: TObject);
     procedure FormCreate(Sender: TObject);
+    procedure ButtonMouseDown(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y: Integer);
   private
     Characters: Array[32..127] of TZXCharacter;
     buttons: Array[0..7,0..7] of TShape;
@@ -28,8 +30,10 @@ type
     CurrentCharacter: Integer;
     procedure SetButtonSize;
     procedure SetCharacter(c: Integer; d: TZXCharacter);
+    procedure UpdateCharacter(c: Integer);
     procedure RedrawCharacter(c: Integer);
     procedure PreviewImageClick(Sender: TObject);
+    procedure GetButtonPosition(const button: TShape; var x: Integer; var y: Integer);
   public
 
   end;
@@ -96,6 +100,20 @@ begin
   end;
 end;
 
+function ColourContrast(incol: TColor): TColor;
+const
+  gamma = 2.2;
+var
+  r,g,b,L: Double;
+begin
+  r := Red(incol) / 255;
+  g := Green(incol) / 255;
+  b := Blue(incol) / 255;
+  L := 0.2126 * power(R, gamma) + 0.7152 * power(G, gamma) + 0.0722 * power(B, gamma);
+  if L > power(0.5, gamma) then Result := clBlack
+  else Result := clWhite;
+end;
+
 { TfrmMain }
 
 procedure TfrmMain.FormCreate(Sender: TObject);
@@ -109,7 +127,7 @@ begin
     begin
       buttons[x,y] := TShape.Create(Self);
       buttons[x,y].Parent := buttonPanel;
-      //buttons[x,y].OnMouseDown := @ButtonMouseDown;
+      buttons[x,y].OnMouseDown := @ButtonMouseDown;
       buttons[x,y].Brush.Color := Paper;
       buttons[x,y].Pen.Color := Paper;
       pixels[x,y] := 0;
@@ -217,8 +235,75 @@ begin
     s := DecToBin(Characters[c][i]);
     for j := 1 to 8 do
     begin
+      pixels[j-1,i] := StrToInt(s[j]);
       if s[j] = '1' then buttons[j-1,i].Brush.Color := clBlack
       else buttons[j-1,i].Brush.Color := clWhite;
+    end;
+  end;
+end;
+
+procedure TfrmMain.UpdateCharacter(c: Integer);
+var
+  i,j: Integer;
+  s: String;
+  b: Byte;
+  d: TZXCharacter;
+begin
+  for i := 0 to 7 do
+  begin
+    s := '';
+    for j := 0 to 7 do
+    begin
+      s := s + pixels[j,i].ToString;
+    end;
+    b := BinToDec(s);
+    d[i] := b;
+  end;
+  Characters[c] := d;
+end;
+
+procedure TfrmMain.ButtonMouseDown(Sender: TObject; Button: TMouseButton;
+  Shift: TShiftState; X, Y: Integer);
+var
+  cc: TColor;
+  i,j: Integer;
+  p: TPoint;
+begin
+  if Button = mbLeft then
+  begin
+    cc := (Sender as TShape).Brush.Color;
+    if cc = Paper then
+    begin
+      (Sender as TShape).Brush.Color := Ink;
+      GetButtonPosition(Sender as TShape,i,j);
+      if (i > -1) and (j > -1) then pixels[i,j] := 1;
+    end
+    else
+    begin
+      (Sender as TShape).Brush.Color := Paper;
+      GetButtonPosition(Sender as TShape,i,j);
+      if (i > -1) and (j > -1) then pixels[i,j] := 0;
+    end;
+    UpdateCharacter(CurrentCharacter);
+    RedrawCharacter(CurrentCharacter);
+  end;
+end;
+
+procedure TfrmMain.GetButtonPosition(const button: TShape; var x: Integer; var y: Integer);
+var
+  i,j: Integer;
+begin
+  x := -1;
+  y := -1;
+  for i := 0 to 7 do
+  begin
+    for j := 0 to 7 do
+    begin
+      if button = buttons[i,j] then
+      begin
+        x := i;
+        y := j;
+      end;
     end;
   end;
 end;
@@ -228,19 +313,21 @@ var
   bmp: graphics.TBitmap;
   i,j: Integer;
   s: String;
-  p: TColor;
+  p,fg,bg: TColor;
 begin
+  fg := clBtnText;
+  bg := clBtnFace;
   bmp := graphics.TBitMap.Create;
   bmp.Width := 8;
   bmp.Height := 8;
-  if c = CurrentCharacter then p := RGB(255,0,255)
-  else p := clWhite;
+  if c = CurrentCharacter then p := clHighlight
+  else p := ColourContrast(fg);
   for i := 0 to 7 do
   begin
     s := DecToBin(Characters[c][i]);
     for j := 1 to 8 do
     begin
-      if s[j] = '1' then bmp.Canvas.pixels[j-1,i] := clBlack
+      if s[j] = '1' then bmp.Canvas.pixels[j-1,i] := fg
       else bmp.Canvas.pixels[j-1,i] := p;
     end;
   end;
